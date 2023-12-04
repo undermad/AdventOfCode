0557bf0e67f40608b0e234e65dc12f931c9f4b56
Dominik Tworek <dtworek94@gmail.com>
2023-12-04 00:31:29 +0000

    advent of code 2023 day 2 part 1

---
+++ src/Day02.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class Day02 {
+
+    public static void main(String[] args) {
+
+        List<String> colors = List.of("green", "blue", "red");
+
+        try (FileReader fr = new FileReader(".\\day02.txt");
+             BufferedReader br = new BufferedReader(fr)) {
+
+
+            String line = br.readLine();
+            int result = 0;
+
+            while (line != null) {
+
+                int gameId = extractGameId(line);
+                System.out.println("GAME ID: " + gameId);
+
+                int green = 0;
+                int blue = 0;
+                int red = 0;
+                boolean isGamePossible = true;
+
+                List<String> gameSets = extractGameSets(line);
+                for (int i = 0; i < gameSets.size(); i++) {
+                    String[] elements = gameSets.get(i).strip().split(",");
+
+                    for (int j = 0; j < elements.length; j++) {
+                        System.out.println("____________");
+                        System.out.println("SET NO: " + j);
+                        String cube = elements[j].strip();
+                        green = 0;
+                        red = 0;
+                        blue = 0;
+                        if (cube.contains("green")) {
+                            green += getIntFromCube(cube);
+                            System.out.println("GREEN: " + green);
+                        } else if (cube.contains("red")) {
+                            red += getIntFromCube(cube);
+                            System.out.println("RED: " + red);
+                        } else if (cube.contains("blue")) {
+                            blue += getIntFromCube(cube);
+                            System.out.println("BLUE: " + blue);
+                        }
+
+                        if(!validateGame(green,red,blue)){
+                            System.out.println("GAME WITH ID: " + gameId + " IS NOT POSSIBLE DUE TO THE SET NO: " + j);
+                            isGamePossible = false;
+                        }
+                    }
+
+
+                }
+
+                if (isGamePossible) {
+                    result += gameId;
+                }
+
+                System.out.println("******************");
+                System.out.println("******************");
+                System.out.println();
+                line = br.readLine();
+            }
+            System.out.println(result);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    private static boolean validateGame(int green, int red, int blue) {
+        if (green > 13 || red > 12 || blue > 14) {
+            return false;
+        }
+        return true;
+    }
+
+    private static int getIntFromCube(String cube) {
+        return Integer.parseInt(cube.substring(0, 2).strip());
+    }
+
+    private static List<String> extractGameSets(String line) {
+        List<String> choppedLine = List.of(line.split(":"));
+        return List.of(choppedLine.get(1).split(";"));
+
+    }
+
+    private static int extractGameId(String line) {
+        List<String> choppedLine = List.of(line.split(":"));
+        return Integer.parseInt(choppedLine.get(0).substring(5));
+    }
+
+}
