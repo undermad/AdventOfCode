a7a153a3dd6759603cd384d86fc4933a8483b158
Dominik Tworek <dtworek94@gmail.com>
2023-12-10 21:09:32 +0000

    advent of code 2023 day 10 part 2

--- src/Day10.java
+++ src/Day10.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,17 +8,20 @@ public class Day10 {
 
     public static void main(String[] args) throws IOException {
         Maze maze = new Maze();
+        maze.createBorders();
         maze.printMaze();
-        System.out.println(maze.countSteps() / 2);
+        System.out.println(maze.countInnerTiles());
+
+
+//        int farthest = maze.countSteps() / 2;
+//        System.out.println("Part 1: " + farthest);
     }
 
     static class Maze {
         List<String> input = extractMaze();
-        char[][] maze = new char[input.size()][input.get(0).length()];
-        int startX;
-        int startY;
-        int currentPositionX;
-        int currentPositionY;
+        Tile[][] maze = new Tile[input.size()][input.get(0).length()];
+        int startX, startY;
+        int currentPositionX, currentPositionY;
         int facingDirection;
         int steps;
 
@@ -26,7 +30,7 @@ public class Day10 {
             for (int row = 0; row < input.size(); row++) {
                 char[] choppedLine = input.get(row).toCharArray();
                 for (int col = 0; col < choppedLine.length; col++) {
-                    this.maze[row][col] = choppedLine[col];
+                    this.maze[row][col] = new Tile(choppedLine[col]);
                     if (choppedLine[col] == 'S') {
                         this.startX = col;
                         this.startY = row;
@@ -40,9 +44,43 @@ public class Day10 {
             this.steps = 0;
         }
 
-        public int countSteps() {
+        public int countInnerTiles() {
+            boolean isIn = false;
+            int innerTiles = 0;
+
+            for (int row = 0; row < maze.length; row++) {
+                isIn = false;
+
+                for (int col = 0; col < maze[row].length; col++) {
+                    Tile currentTile = maze[row][col];
+
+                    if (isIn && !currentTile.isBorder) innerTiles++;
+                    else if (isIn && currentTile.isBorder && currentTile.pipeType == '|')
+                        isIn = false;
+                    else if (isIn && currentTile.isBorder && currentTile.pipeType == '7' && currentTile.facingDirection == FacingDirection.WEST)
+                        isIn = false;
+                    else if(isIn && currentTile.isBorder && currentTile.pipeType == 'J' && currentTile.facingDirection == FacingDirection.NORTH)
+                        isIn = false;
+                    else if (!isIn && currentTile.isBorder) isIn = true;
+                }
+            }
+            return innerTiles;
+        }
+
+        public void createBorders() {
             step();
-            while (maze[currentPositionY][currentPositionX] != 'S') {
+            while (maze[currentPositionY][currentPositionX].pipeType != 'S') {
+                maze[currentPositionY][currentPositionX].isBorder = true;
+                step();
+            }
+            maze[currentPositionY][currentPositionX].isBorder = true;
+        }
+
+
+        public int countSteps() {
+            this.steps = 0;
+            step();
+            while (maze[currentPositionY][currentPositionX].pipeType != 'S') {
                 step();
             }
             return steps;
@@ -51,7 +89,8 @@ public class Day10 {
 
         public void step() {
             if (facingDirection == FacingDirection.EAST) {
-                char nextPipe = maze[currentPositionY][currentPositionX + 1];
+                char nextPipe = maze[currentPositionY][currentPositionX + 1].pipeType;
+                maze[currentPositionY][currentPositionX].facingDirection = facingDirection;
                 currentPositionX++;
                 Rotation rotationDirection = switch (nextPipe) {
                     case 'J' -> Rotation.ROTATE_LEFT;
@@ -60,7 +99,8 @@ public class Day10 {
                 };
                 rotate(rotationDirection);
             } else if (facingDirection == FacingDirection.SOUTH) {
-                char nextPipe = maze[currentPositionY + 1][currentPositionX];
+                char nextPipe = maze[currentPositionY + 1][currentPositionX].pipeType;
+                maze[currentPositionY][currentPositionX].facingDirection = facingDirection;
                 currentPositionY++;
                 Rotation rotationDirection = switch (nextPipe) {
                     case 'J' -> Rotation.ROTATE_RIGHT;
@@ -69,7 +109,8 @@ public class Day10 {
                 };
                 rotate(rotationDirection);
             } else if (facingDirection == FacingDirection.WEST) {
-                char nextPipe = maze[currentPositionY][currentPositionX - 1];
+                char nextPipe = maze[currentPositionY][currentPositionX - 1].pipeType;
+                maze[currentPositionY][currentPositionX].facingDirection = facingDirection;
                 currentPositionX--;
                 Rotation rotationDirection = switch (nextPipe) {
                     case 'F' -> Rotation.ROTATE_LEFT;
@@ -78,7 +119,8 @@ public class Day10 {
                 };
                 rotate(rotationDirection);
             } else if (facingDirection == FacingDirection.NORTH) {
-                char nextPipe = maze[currentPositionY - 1][currentPositionX];
+                char nextPipe = maze[currentPositionY - 1][currentPositionX].pipeType;
+                maze[currentPositionY][currentPositionX].facingDirection = facingDirection;
                 currentPositionY--;
                 Rotation rotationDirection = switch (nextPipe) {
                     case 'F' -> Rotation.ROTATE_RIGHT;
@@ -104,9 +146,13 @@ public class Day10 {
         }
 
         public void printMaze() {
-            for (char[] row : maze) {
-                for (char col : row) {
-                    System.out.print(col);
+            for (Tile[] row : maze) {
+                for (Tile col : row) {
+                    if (col.isBorder)
+                        System.out.print("*");
+                    else
+                        System.out.print(col.pipeType);
+
                 }
                 System.out.println();
             }
@@ -129,6 +175,16 @@ public class Day10 {
         ROTATE_RIGHT,
         ROTATE_LEFT,
         DONT_ROTATE,
+    }
+
+    static class Tile {
+        public char pipeType;
+        public boolean isBorder;
+        public int facingDirection;
+
+        public Tile(char pipeType) {
+            this.pipeType = pipeType;
+        }
     }
 
 
